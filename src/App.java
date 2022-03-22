c0389e7faae0bbee63d5590d3cbbd3391d8fe31c
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2022-03-23 01:12:53 +0300

    v2

--- src/App.java
+++ src/App.java
@@ -1,16 +1,28 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner fileObserver = new Scanner(
-                new File("C:\\Users\\Berke\\Documents\\Code\\Java\\SE3751\\src\\01-January.csv"));
-        fileObserver.useDelimiter(" ");
-        while (fileObserver.hasNext()) {
+                new File("C:\\Users\\bprld\\Documents\\GitHub\\SE375Labs\\src\\01-January.csv"));
+        fileObserver.useDelimiter(",");
+        // while (fileObserver.hasNext()) {
+        fileObserver.nextLine();
+        // System.out.println(fileObserver.next());
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 2; i < 14; i++) {
+            fileObserver.next();
+            arr.add(fileObserver.nextInt());
+            // System.out.println(fileObserver.next());
             fileObserver.nextLine();
-            System.out.println(fileObserver.next());
-
+            // System.out.println(fileObserver.next());
         }
+        System.out.println("shee");
+        for (int a = 0; a < arr.size(); a++) {
+            System.out.println(arr.get(a));
+        }
+        // }
         fileObserver.close();
     }
 }
