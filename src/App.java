061237e85df44c7a9b9b26d0de976596399e4057
berkeparildar <berkeparildar@gmail.com>
2022-03-25 01:26:08 +0300

    fix

--- src/App.java
+++ src/App.java
@@ -23,23 +23,104 @@ public class App {
         }
     }
 
-    public static int mothlySale(ArrayList<Integer> price, ArrayList<Integer> array) {// method calculating in-store
+    public static int mothlySale(ArrayList<Integer> price, ArrayList<Integer> array) {// method calculating
         int sum = 0; // sales for a month.
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < array.size(); i++) {
             int productSales = price.get(i) * array.get(i);
             sum += productSales;
         }
         return sum;
     }
 
+    public static int yearlySaleAmount(ArrayList<ArrayList<Integer>> array1, ArrayList<ArrayList<Integer>> array2) {
+        int sum = 0;
+        for (int i = 0; i < array1.size(); i++) {
+            for (int k = 0; k < array2.size(); k++) {
+                int p = array1.get(i).get(k) + array2.get(i).get(k);
+                sum += p;
+            }
+        }
+        return sum;
+    }
+
+    public static int yearlySalePrice(ArrayList<ArrayList<Integer>> array, Variables var) {
+        int sum = 0;
+        sum += mothlySale(var.getJanuaryPrice(), array.get(0));
+        sum += mothlySale(var.getFebruaryPrice(), array.get(1));
+        sum += mothlySale(var.getMarchPrice(), array.get(2));
+        sum += mothlySale(var.getAprilPrice(), array.get(3));
+        sum += mothlySale(var.getMayPrice(), array.get(4));
+        sum += mothlySale(var.getJunePrice(), array.get(5));
+        sum += mothlySale(var.getJulyPrice(), array.get(6));
+        sum += mothlySale(var.getAugustPrice(), array.get(7));
+        sum += mothlySale(var.getSeptemberPrice(), array.get(8));
+        sum += mothlySale(var.getOctoberPrice(), array.get(9));
+        sum += mothlySale(var.getNovemberPrice(), array.get(10));
+        sum += mothlySale(var.getDecemberPrice(), array.get(11));
+        return sum;
+    }
+
     public static void main(String[] args) throws Exception {
         // Variables for holding content of January.
         Variables var = new Variables();
+
+        // creating these arraylists which will store the "in-store" and online prices
+        // of all months.
+        // I created these because I only wanted 1 method for yearly sales.
+        ArrayList<ArrayList<Integer>> inStore = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> online = new ArrayList<>();
+
+        // start reading all 12 .csv files
         readCSV(var.getJanuaryPath(), var.getJanuaryPrice(), var.getJanuaryStore(), var.getJanuaryOnline());
         readCSV(var.getFebruaryPath(), var.getFebruaryPrice(), var.getFebruaryStore(), var.getFebruaryOnline());
-        System.out.println(mothlySale(var.getJanuaryPrice(), var.getJanuaryStore()));// Answer for question 2
-        System.out.println(mothlySale(var.getJanuaryPrice(), var.getJanuaryOnline()));// Answer for question 3
-        System.out.println(mothlySale(var.getFebruaryPrice(), var.getFebruaryStore()));
+        readCSV(var.getMarchPath(), var.getMarchPrice(), var.getMarchStore(), var.getMarchOnline());
+        readCSV(var.getAprilPath(), var.getAprilPrice(), var.getAprilStore(), var.getAprilOnline());
+        readCSV(var.getMayPath(), var.getMayPrice(), var.getMayStore(), var.getMayOnline());
+        readCSV(var.getJunePath(), var.getJunePrice(), var.getJuneStore(), var.getJuneOnline());
+        readCSV(var.getJulyPath(), var.getJulyPrice(), var.getJulyStore(), var.getJulyOnline());
+        readCSV(var.getAugustPath(), var.getAugustPrice(), var.getAugustStore(), var.getAugustOnline());
+        readCSV(var.getSeptemberPath(), var.getSeptemberPrice(), var.getSeptemberStore(), var.getSeptemberOnline());
+        readCSV(var.getOctoberPath(), var.getOctoberPrice(), var.getOctoberStore(), var.getOctoberOnline());
+        readCSV(var.getNovemberPath(), var.getNovemberPrice(), var.getNovemberStore(), var.getNovemberOnline());
+        readCSV(var.getDecemberPath(), var.getDecemberPrice(), var.getDecemberStore(), var.getDecemberOnline());
 
+        // adding store values to the store array
+        // which will be used on yearly sale method
+        inStore.add(var.getJanuaryStore());
+        inStore.add(var.getFebruaryStore());
+        inStore.add(var.getMarchStore());
+        inStore.add(var.getAprilStore());
+        inStore.add(var.getMayStore());
+        inStore.add(var.getJuneStore());
+        inStore.add(var.getJulyStore());
+        inStore.add(var.getAugustStore());
+        inStore.add(var.getSeptemberStore());
+        inStore.add(var.getOctoberStore());
+        inStore.add(var.getNovemberStore());
+        inStore.add(var.getDecemberStore());
+
+        // adding store values to the store array
+        // which will be used on yearly sale method
+        online.add(var.getJanuaryOnline());
+        online.add(var.getFebruaryOnline());
+        online.add(var.getMarchOnline());
+        online.add(var.getAprilOnline());
+        online.add(var.getMayOnline());
+        online.add(var.getJuneOnline());
+        online.add(var.getJulyOnline());
+        online.add(var.getAugustOnline());
+        online.add(var.getSeptemberOnline());
+        online.add(var.getOctoberOnline());
+        online.add(var.getNovemberOnline());
+        online.add(var.getDecemberOnline());
+
+        System.out.println("Year sale: " + yearlySaleAmount(inStore, online) + " amount");// Answer for question 1
+        System.out.println("Monthly in-store sales of all products in the January are: "
+                + mothlySale(var.getJanuaryPrice(), var.getJanuaryStore()) + "TL");// Answer for question2
+        System.out.println("Monthly online sales of all products in the January are: "
+                + mothlySale(var.getJanuaryPrice(), var.getJanuaryOnline()) + "TL");// Answer for question3
+        // answer for question 4
+        System.out.println("The total in-store sales for a year are: " + yearlySalePrice(inStore, var) + "TL");
+        System.out.println("The total online sales for a year are: " + yearlySalePrice(online, var) + "TL");
     }
 }
--- src/Variables.java
+++ src/Variables.java
@@ -7,7 +7,7 @@ public class Variables {
     String aprilPath = "C:\\Users\\Berke\\Documents\\Code\\Java\\SE375Labs\\src\\04-April.csv";
     String mayPath = "C:\\Users\\Berke\\Documents\\Code\\Java\\SE375Labs\\src\\05-May.csv";
     String junePath = "C:\\Users\\Berke\\Documents\\Code\\Java\\SE375Labs\\src\\06-June.csv";
-    String julyPath = "C:\\Users\\Berke\\Documents\\Code\\Java\\SE375Labs\\src\\07-March.csv";
+    String julyPath = "C:\\Users\\Berke\\Documents\\Code\\Java\\SE375Labs\\src\\07-July.csv";
     String augustPath = "C:\\Users\\Berke\\Documents\\Code\\Java\\SE375Labs\\src\\08-August.csv";
     String septemberPath = "C:\\Users\\Berke\\Documents\\Code\\Java\\SE375Labs\\src\\09-September.csv";
     String octoberPath = "C:\\Users\\Berke\\Documents\\Code\\Java\\SE375Labs\\src\\10-October.csv";
@@ -19,6 +19,36 @@ public class Variables {
     ArrayList<Integer> februaryPrice = new ArrayList<>();
     ArrayList<Integer> februaryStore = new ArrayList<>();
     ArrayList<Integer> februaryOnline = new ArrayList<>();
+    ArrayList<Integer> marchPrice = new ArrayList<>();
+    ArrayList<Integer> marchStore = new ArrayList<>();
+    ArrayList<Integer> marchOnline = new ArrayList<>();
+    ArrayList<Integer> aprilPrice = new ArrayList<>();
+    ArrayList<Integer> aprilStore = new ArrayList<>();
+    ArrayList<Integer> aprilOnline = new ArrayList<>();
+    ArrayList<Integer> mayPrice = new ArrayList<>();
+    ArrayList<Integer> mayStore = new ArrayList<>();
+    ArrayList<Integer> mayOnline = new ArrayList<>();
+    ArrayList<Integer> junePrice = new ArrayList<>();
+    ArrayList<Integer> juneStore = new ArrayList<>();
+    ArrayList<Integer> juneOnline = new ArrayList<>();
+    ArrayList<Integer> julyPrice = new ArrayList<>();
+    ArrayList<Integer> julyStore = new ArrayList<>();
+    ArrayList<Integer> julyOnline = new ArrayList<>();
+    ArrayList<Integer> augustPrice = new ArrayList<>();
+    ArrayList<Integer> augustStore = new ArrayList<>();
+    ArrayList<Integer> augustOnline = new ArrayList<>();
+    ArrayList<Integer> septemberPrice = new ArrayList<>();
+    ArrayList<Integer> septemberStore = new ArrayList<>();
+    ArrayList<Integer> septemberOnline = new ArrayList<>();
+    ArrayList<Integer> octoberPrice = new ArrayList<>();
+    ArrayList<Integer> octoberStore = new ArrayList<>();
+    ArrayList<Integer> octoberOnline = new ArrayList<>();
+    ArrayList<Integer> novemberPrice = new ArrayList<>();
+    ArrayList<Integer> novemberStore = new ArrayList<>();
+    ArrayList<Integer> novemberOnline = new ArrayList<>();
+    ArrayList<Integer> decemberPrice = new ArrayList<>();
+    ArrayList<Integer> decemberStore = new ArrayList<>();
+    ArrayList<Integer> decemberOnline = new ArrayList<>();
 
     public String getJanuaryPath() {
         return januaryPath;
@@ -80,24 +110,144 @@ public class Variables {
         return januaryOnline;
     }
 
-    /**
-     * @return the februaryPrice
-     */
     public ArrayList<Integer> getFebruaryPrice() {
         return februaryPrice;
     }
 
-    /**
-     * @return the februaryStore
-     */
     public ArrayList<Integer> getFebruaryStore() {
         return februaryStore;
     }
 
-    /**
-     * @return the februaryOnline
-     */
     public ArrayList<Integer> getFebruaryOnline() {
         return februaryOnline;
+    }
+
+    public ArrayList<Integer> getMarchPrice() {
+        return marchPrice;
+    }
+
+    public ArrayList<Integer> getMarchStore() {
+        return marchStore;
+    }
+
+    public ArrayList<Integer> getMarchOnline() {
+        return marchOnline;
+    }
+
+    public ArrayList<Integer> getAprilPrice() {
+        return aprilPrice;
+    }
+
+    public ArrayList<Integer> getAprilStore() {
+        return aprilStore;
+    }
+
+    public ArrayList<Integer> getAprilOnline() {
+        return aprilOnline;
+    }
+
+    public ArrayList<Integer> getMayPrice() {
+        return mayPrice;
+    }
+
+    public ArrayList<Integer> getMayStore() {
+        return mayStore;
+    }
+
+    public ArrayList<Integer> getMayOnline() {
+        return mayOnline;
+    }
+
+    public ArrayList<Integer> getJunePrice() {
+        return junePrice;
+    }
+
+    public ArrayList<Integer> getJuneStore() {
+        return juneStore;
+    }
+
+    public ArrayList<Integer> getJuneOnline() {
+        return juneOnline;
+    }
+
+    /**
+     * @return the julyPrice
+     */
+    public ArrayList<Integer> getJulyPrice() {
+        return julyPrice;
+    }
+
+    /**
+     * @return the julyStore
+     */
+    public ArrayList<Integer> getJulyStore() {
+        return julyStore;
+    }
+
+    /**
+     * @return the julyOnline
+     */
+    public ArrayList<Integer> getJulyOnline() {
+        return julyOnline;
+    }
+
+    public ArrayList<Integer> getAugustPrice() {
+        return augustPrice;
+    }
+
+    public ArrayList<Integer> getAugustStore() {
+        return augustStore;
+    }
+
+    public ArrayList<Integer> getAugustOnline() {
+        return augustOnline;
+    }
+
+    public ArrayList<Integer> getSeptemberPrice() {
+        return septemberPrice;
+    }
+
+    public ArrayList<Integer> getSeptemberStore() {
+        return septemberStore;
+    }
+
+    public ArrayList<Integer> getSeptemberOnline() {
+        return septemberOnline;
+    }
+
+    public ArrayList<Integer> getOctoberPrice() {
+        return octoberPrice;
+    }
+
+    public ArrayList<Integer> getOctoberStore() {
+        return octoberStore;
+    }
+
+    public ArrayList<Integer> getOctoberOnline() {
+        return octoberOnline;
+    }
+
+    public ArrayList<Integer> getNovemberPrice() {
+        return novemberPrice;
+    }
+
+    public ArrayList<Integer> getNovemberStore() {
+        return novemberStore;
+    }
+
+    public ArrayList<Integer> getNovemberOnline() {
+        return novemberOnline;
+    }
+
+    public ArrayList<Integer> getDecemberPrice() {
+        return decemberPrice;
+    }
+
+    public ArrayList<Integer> getDecemberStore() {
+        return decemberStore;
+    }
+
+    public ArrayList<Integer> getDecemberOnline() {
+        return decemberOnline;
     }
 }
