e8b9b0118555e2fcb3727c709bdc8a186f8192d2
berkeparildar <berkeparildar@gmail.com>
2022-03-27 03:49:53 +0300

    this time on threads, but lost

--- src/App.java
+++ src/App.java
@@ -4,61 +4,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App implements Runnable {
+    Variables var = new Variables();
     private static Scanner fileObserver;
+    String month;
+    String path;
+    ArrayList<Integer> price = new ArrayList<>();
+    ArrayList<Integer> store = new ArrayList<>();
+    ArrayList<Integer> online = new ArrayList<>();
+    ArrayList<Integer> totalStore = new ArrayList<>();
+    ArrayList<Integer> totalOnline = new ArrayList<>();
+    ArrayList<Integer> total = new ArrayList<>();
 
-    public App(String month, String path, ArrayList<Integer> array, ArrayList<Integer> array2,
-            ArrayList<Integer> array3) {
-        int sumAllStore = 0;
-        int sumAllOnline = 0;
-        readCSV(path, array, array2, array3);
-        System.out.println(month + " store sale: " + mothlySale(array, array2));
-        System.out.println(month + " online sale: " + mothlySale(array, array3));
+    public App(String tMonth, String tPath, ArrayList<Integer> tArray1, ArrayList<Integer> tArray2,
+            ArrayList<Integer> tArray3, ArrayList<Integer> tTotalStore, ArrayList<Integer> tTotalOnline,
+            ArrayList<Integer> tTotalOfAll) {
+        month = tMonth;
+        path = tPath;
+        price = tArray1;
+        store = tArray2;
+        online = tArray3;
+        totalStore = tTotalStore;
+        totalOnline = tTotalOnline;
+        total = tTotalOfAll;
     }
 
     public void run() {
-
-        // Variables for holding content of January.
-        Variables var = new Variables();
-        // creating these arraylists which will store the "in-store" and Online prices
-        // of all months.
-        // I created these because I only wanted 1 method for yearly sales.
-        ArrayList<ArrayList<Integer>> inStore = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> Online = new ArrayList<>();
-
-        // adding store values to the store array
-        // which will be used on yearly sale method
-        inStore.add(var.getJanuaryStore());
-        inStore.add(var.getFebruaryStore());
-        inStore.add(var.getMarchStore());
-        inStore.add(var.getAprilStore());
-        inStore.add(var.getMayStore());
-        inStore.add(var.getJuneStore());
-        inStore.add(var.getJulyStore());
-        inStore.add(var.getAugustStore());
-        inStore.add(var.getSeptemberStore());
-        inStore.add(var.getOctoberStore());
-        inStore.add(var.getNovemberStore());
-        inStore.add(var.getDecemberStore());
-
-        // adding store values to the store array
-        // which will be used on yearly sale method
-        Online.add(var.getJanuaryOnline());
-        Online.add(var.getFebruaryOnline());
-        Online.add(var.getMarchOnline());
-        Online.add(var.getAprilOnline());
-        Online.add(var.getMayOnline());
-        Online.add(var.getJuneOnline());
-        Online.add(var.getJulyOnline());
-        Online.add(var.getAugustOnline());
-        Online.add(var.getSeptemberOnline());
-        Online.add(var.getOctoberOnline());
-        Online.add(var.getNovemberOnline());
-        Online.add(var.getDecemberOnline());
-
+        readCSV(path, price, store, online);
+        int monthlyStoreSale = monthlyStoreSale(price, store);
+        System.out.println(month + " store sale: " + monthlyStoreSale);
+        int monthlyOnlineSale = monthlyOnlineSale(price, online);
+        System.out.println(month + " online sale: " + monthlyOnlineSale);
+        totalStore.add(monthlyStoreSale);
+        totalOnline.add(monthlyOnlineSale);
+        total.add(monthlyOnlineSale);
+        total.add(monthlyStoreSale);
     }
 
-    public static void readCSV(String path, ArrayList<Integer> price, ArrayList<Integer> store,
-            ArrayList<Integer> Online) {
+    public static void readCSV(String path, ArrayList<Integer> rPrice, ArrayList<Integer> rStore,
+            ArrayList<Integer> rOnline) {
         try {
             fileObserver = new Scanner(new File(path)).useDelimiter(",");
         } catch (FileNotFoundException e) {
@@ -68,15 +51,24 @@ public class App implements Runnable {
         while (fileObserver.hasNextLine()) {
             String row = fileObserver.nextLine();
             String[] contents = row.split(",");
-            price.add(Integer.parseInt(contents[1]));
-            store.add(Integer.parseInt(contents[2]));
-            Online.add(Integer.parseInt(contents[3]));
+            rPrice.add(Integer.parseInt(contents[1]));
+            rStore.add(Integer.parseInt(contents[2]));
+            rOnline.add(Integer.parseInt(contents[3]));
         }
     }
 
-    public static int mothlySale(ArrayList<Integer> price, ArrayList<Integer> array) {// method calculating
+    public static int monthlyStoreSale(ArrayList<Integer> price, ArrayList<Integer> array) {// method calculating
         int sum = 0; // sales for a month.
-        for (int i = 0; i < array.size(); i++) {
+        for (int i = 0; i < 12; i++) {
+            int productSales = price.get(i) * array.get(i);
+            sum += productSales;
+        }
+        return sum;
+    }
+
+    public static int monthlyOnlineSale(ArrayList<Integer> price, ArrayList<Integer> array) {// method calculating
+        int sum = 0; // sales for a month.
+        for (int i = 0; i < 12; i++) {
             int productSales = price.get(i) * array.get(i);
             sum += productSales;
         }
@@ -91,23 +83,6 @@ public class App implements Runnable {
                 sum += p;
             }
         }
-        return sum;
-    }
-
-    public static int yearlySalePrice(ArrayList<ArrayList<Integer>> array, Variables var) {
-        int sum = 0;
-        sum += mothlySale(var.getJanuaryPrice(), array.get(0));
-        sum += mothlySale(var.getFebruaryPrice(), array.get(1));
-        sum += mothlySale(var.getMarchPrice(), array.get(2));
-        sum += mothlySale(var.getAprilPrice(), array.get(3));
-        sum += mothlySale(var.getMayPrice(), array.get(4));
-        sum += mothlySale(var.getJunePrice(), array.get(5));
-        sum += mothlySale(var.getJulyPrice(), array.get(6));
-        sum += mothlySale(var.getAugustPrice(), array.get(7));
-        sum += mothlySale(var.getSeptemberPrice(), array.get(8));
-        sum += mothlySale(var.getOctoberPrice(), array.get(9));
-        sum += mothlySale(var.getNovemberPrice(), array.get(10));
-        sum += mothlySale(var.getDecemberPrice(), array.get(11));
         return sum;
     }
 }
--- src/runmain.java
+++ src/runmain.java
@@ -1,59 +1,91 @@
+import java.util.ArrayList;
+
 public class runmain {
+
         public static void main(String[] args) throws InterruptedException {
                 Variables var = new Variables();
+                ArrayList<Integer> allStore = new ArrayList<>();
+                ArrayList<Integer> allOnline = new ArrayList<>();
+                ArrayList<Integer> totalofAll = new ArrayList<>();
+                int storeSum = 0;
+                int onlineSum = 0;
+                int total = 0;
                 App January = new App("January", var.getJanuaryPath(), var.getJanuaryPrice(), var.getJanuaryStore(),
-                                var.getJanuaryOnline());
+                                var.getJanuaryOnline(), allStore, allOnline, totalofAll);
                 Thread t1 = new Thread(January);
                 t1.start();
-
-                App February = new App("February", var.getFebruaryPath(), var.getFebruaryPrice(),
+                t1.join();
+                App February = new App("February", var.getFebruaryPath(),
+                                var.getFebruaryPrice(),
                                 var.getFebruaryStore(),
-                                var.getFebruaryOnline());
+                                var.getFebruaryOnline(), allStore, allOnline, totalofAll);
                 Thread t2 = new Thread(February);
-                t1.join(2000);
                 t2.start();
-                App March = new App("March", var.getMarchPath(), var.getMarchPrice(), var.getMarchStore(),
-                                var.getMarchOnline());
+                t2.join();
+                App March = new App("March", var.getMarchPath(), var.getMarchPrice(),
+                                var.getMarchStore(),
+                                var.getMarchOnline(), allStore, allOnline, totalofAll);
                 Thread t3 = new Thread(March);
                 t3.start();
-                App april = new App("April", var.getAprilPath(), var.getAprilPrice(), var.getAprilStore(),
-                                var.getAprilOnline());
-                Thread t4 = new Thread(april);
-                t4.start();
-                App May = new App("May", var.getMayPath(), var.getMayPrice(), var.getMayStore(),
-                                var.getMayOnline());
-                Thread t5 = new Thread(May);
-                t5.start();
-                App June = new App("June", var.getJunePath(), var.getJunePrice(), var.getJuneStore(),
-                                var.getJuneOnline());
-                Thread t6 = new Thread(June);
-                t6.start();
-                App July = new App("July", var.getJulyPath(), var.getJulyPrice(), var.getJulyStore(),
-                                var.getJulyOnline());
-                Thread t7 = new Thread(July);
-                t7.start();
-                App August = new App("August", var.getAugustPath(), var.getAugustPrice(), var.getAugustStore(),
-                                var.getAugustOnline());
-                Thread t8 = new Thread(August);
-                t8.start();
-                App September = new App("September", var.getSeptemberPath(), var.getSeptemberPrice(),
-                                var.getSeptemberStore(),
-                                var.getSeptemberOnline());
-                Thread t9 = new Thread(September);
-                t9.start();
-                App October = new App("October", var.getOctoberPath(), var.getOctoberPrice(), var.getOctoberStore(),
-                                var.getOctoberOnline());
-                Thread t10 = new Thread(October);
-                t10.start();
-                App November = new App("November", var.getNovemberPath(), var.getNovemberPrice(),
-                                var.getNovemberStore(),
-                                var.getNovemberOnline());
-                Thread t11 = new Thread(November);
-                t11.start();
-                App December = new App("December", var.getDecemberPath(), var.getDecemberPrice(),
-                                var.getDecemberStore(),
-                                var.getDecemberOnline());
-                Thread t12 = new Thread(December);
-                t12.start();
+
+                // App april = new App("April", var.getAprilPath(), var.getAprilPrice(),
+                // var.getAprilStore(),
+                // var.getAprilOnline(), allStore, allOnline, totalofAll);
+                // Thread t4 = new Thread(april);
+                // t4.run();
+
+                // App May = new App("May", var.getMayPath(), var.getMayPrice(),
+                // var.getMayStore(),
+                // var.getMayOnline(), allStore, allOnline, totalofAll);
+                // Thread t5 = new Thread(May);
+
+                // App June = new App("June", var.getJunePath(), var.getJunePrice(),
+                // var.getJuneStore(),
+                // var.getJuneOnline(), allStore, allOnline, totalofAll);
+                // Thread t6 = new Thread(June);
+
+                // App July = new App("July", var.getJulyPath(), var.getJulyPrice(),
+                // var.getJulyStore(),
+                // var.getJulyOnline(), allStore, allOnline, totalofAll);
+                // Thread t7 = new Thread(July);
+
+                // App August = new App("August", var.getAugustPath(), var.getAugustPrice(),
+                // var.getAugustStore(),
+                // var.getAugustOnline(), allStore, allOnline, totalofAll);
+                // Thread t8 = new Thread(August);
+
+                // App September = new App("September", var.getSeptemberPath(),
+                // var.getSeptemberPrice(),
+                // var.getSeptemberStore(),
+                // var.getSeptemberOnline(), allStore, allOnline, totalofAll);
+                // Thread t9 = new Thread(September);
+
+                // App October = new App("October", var.getOctoberPath(), var.getOctoberPrice(),
+                // var.getOctoberStore(),
+                // var.getOctoberOnline(), allStore, allOnline, totalofAll);
+                // Thread t10 = new Thread(October);
+                // App November = new App("November", var.getNovemberPath(),
+                // var.getNovemberPrice(),
+                // var.getNovemberStore(),
+                // var.getNovemberOnline(), allStore, allOnline, totalofAll);
+                // Thread t11 = new Thread(November);
+                // App December = new App("December", var.getDecemberPath(),
+                // var.getDecemberPrice(),
+                // var.getDecemberStore(),
+                // var.getDecemberOnline(), allStore, allOnline, totalofAll);
+                // Thread t12 = new Thread(December);
+
+                // for (int i = 0; i < 24; i++) {
+                // total += totalofAll.get(i);
+                // }
+
+                // for (int i = 0; i < 12; i++) {
+                // storeSum += allStore.get(i);
+                // onlineSum += allOnline.get(i);
+                // }
+                // System.out.println("There are, ");
+                // System.out.println("In-store: $" + storeSum);
+                // System.out.println("Online: $" + onlineSum);
+                // System.out.println("$" + total + " worth of sales for all products");
         }
 }
